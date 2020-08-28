201bfe59e8fca3677f2cd07d6bc9e3142ad5668b
lb15 <40410297+lb15@users.noreply.github.com>
2020-08-28 15:00:26 -0700

    Add files via upload

---
+++ seurat_functions_v2.R
@@ -0,0 +1,138 @@
+### functions for Seurat processing
+options(echo=TRUE)
+qc_plots_stats <- function(seur, basename, version){
+
+        png(paste0(basename,"_",version,"_vlnQC_nodots.png"),height = 800,width=1100)
+        print(VlnPlot(object = seur, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, pt.size = 0,group.by = "orig.ident"))
+        dev.off()
+        
+        png(paste0(basename,"_",version,"_vlnQC_withdots.png"),height = 800,width=1100)
+        print(VlnPlot(object = seur, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3,group.by = "orig.ident"))
+        dev.off()
+        
+        plot1 <- FeatureScatter(seur, feature1 = "nCount_RNA", feature2 = "percent.mt")
+        plot2 <- FeatureScatter(seur, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+        
+        png(paste0(basename,"_",version,"_featurescatter.png"),height = 800,width=1100)
+        print(CombinePlots(plots = list(plot1, plot2)))
+        dev.off()
+        
+        png(paste0(basename,"_",version,"_percentmt_histogram.png"),height = 800,width=1100)
+        hist(seur@meta.data$percent.mt)
+        dev.off()
+        png(paste0(basename,"_",version,"_nCountRNA_histogram.png"),height = 800,width=1100)
+        hist(seur@meta.data$nCount_RNA)
+        dev.off()
+        png(paste0(basename,"_",version,"_nFeatureRNA_histogram.png"),height = 800,width=1100)
+        hist(seur@meta.data$nFeature_RNA)
+        dev.off()
+        
+        mean_percent.mt = mean(seur$percent.mt)
+        median_percent.mt = median(seur$percent.mt)
+        sd_percent.mt = sd(seur$percent.mt)
+        mean_nCount = mean(seur$nCount_RNA)
+        median_nCount= median(seur$nCount_RNA)
+        sd_nCount = sd(seur$nCount_RNA)
+        mean_nFeature = mean(seur$nFeature_RNA)
+        median_nFeature= mean(seur$nFeature_RNA)
+        sd_nFeature=sd(seur$nFeature_RNA)
+        
+        all_stats = list(mean_percent.mt, median_percent.mt, sd_percent.mt, mean_nCount, median_nCount, sd_nCount, mean_nFeature, median_nFeature, sd_nFeature)
+        
+        statsdf = as.data.frame(do.call(rbind, all_stats))
+        stats_print = cbind(statsdf, "Stat"=c("Mean_Percent.mt","Median_Percent.mt", "Standard_deviation_Percent.mt","Mean_nCount","Median_nCount", "Standard_deviation_nCount","Mean_nFeature","Median_nFeature", "Standard_deviation_nFeature"))
+        
+        write.csv(stats_print, file=paste(basename,version,"stats.csv", sep="_"))
+}
+
+run_dr <- function(seur, basename, version, res, num_pcs){
+        if(is.numeric(res)){
+                seur <- FindClusters(seur, resolution = res)
+                umap_plotting(seur, basename,version,res)
+                qc_plotting(seur,basename,version)
+                if(do_marks){
+                        get_marks(seur,basename,version,x)
+                }
+                return(seur)
+        }else{
+                qc_plotting(seur, basename,version)
+                res=c(0.4, 0.8, 1.2, 1.6)
+                for(x in res){
+                        seur <- FindClusters(seur, resolution = x)
+                        umap_plotting(seur, basename, version, x)
+                        if(do_marks){
+                                get_marks(seur,basename,version,x)
+                        }
+                }
+                return(seur)
+        }
+}
+
+umap_plotting <- function(seur,basename,version,res){
+        png(paste(basename, version, "res",res,"UMAP.png",sep="_"),height=800,width=1100)
+        print(DimPlot(seur, reduction = "umap")) 
+        dev.off()
+}
+
+qc_plotting <- function(seur,basename,version){
+        
+        genes_plot=c("nFeature_RNA","nCount_RNA","percent.mt")
+        
+        for(gene in genes_plot){
+                png(paste(basename,version, gene,"plot.png",sep="_"),height = 800,width=1100)
+                print(FeaturePlot(seur,features = gene,pt.size = 2))
+                dev.off()
+        }
+        
+}
+
+get_marks <- function(seur,basename,version,res){
+        seur.markers <- FindAllMarkers(seur, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+        seur.markers.ordered <- arrange(seur.markers, cluster, desc(avg_logFC))
+        
+        write.csv(seur.markers.ordered,file=paste(basename,version,"res",res,"markers.csv",sep="_"))
+}
+
+#######################################
+##check parameters
+
+list_of_analyses <- function(analyses){
+        loa =strsplit(analyses,",")[[1]]
+        list_to_test =sapply(loa, function(x) gsub(" ", "", x, fixed = TRUE))
+        return(list_to_test)
+}
+
+parameter_checks <- function(parameters){
+        horm = parameters$human_mouse
+        MT_filter = parameters$Mt_filter
+        nCount_high = parameters$nCount_high
+        nCount_low = parameters$nCount_low
+        nFeature_high = parameters$nFeature_high
+        nFeature_low = parameters$nFeature_low
+        num_pcs = parameters$PCs
+        res=parameters$resolution
+        doublets = parameters$path_to_doublet_csv 
+        analyses=parameters$analyses
+        
+        check.integer <- function(x){x%%1==0}
+        
+        assertthat::assert_that(horm %in% c("mouse","human"), 
+                                msg="Parameters must contain 'mouse' or 'human' in first column")
+        assertthat::assert_that(is.numeric(MT_filter) | is.na(MT_filter),
+                                msg = "MT_filter must be numeric or NA")
+        assertthat::assert_that(is.numeric(nCount_high) | is.na(nCount_high),
+                                msg = "nCount_high must be numeric or NA")
+        assertthat::assert_that(is.numeric(nFeature_high) | is.na(nFeature_high),
+                                msg = "nFeature_high must be numeric or NA")
+        assertthat::is.number(nCount_low)
+        assertthat::is.number(nFeature_low)
+        assertthat::assert_that(check.integer(num_pcs),
+                                msg = "num_pcs must be an integer")
+        assertthat::assert_that(is.numeric(res) | res == "range",
+                                msg="res must be numeric or 'range'")
+        assertthat::assert_that(is.character(doublets) | is.na(doublets),
+                                msg="doublets must be path to doublet csv file or NA")
+        list_to_test = list_of_analyses(analyses)
+        assertthat::assert_that(sum(!list_to_test %in% c("all","basic","basic_regress_pcmt","basic_regress_cnt","basic_regress_both","sctransform","sctransform_regress","scran"))==0,
+                                msg = "Analyses must be one or more of the following, separated by a comma: 'all', 'basic','basic_regress_pcmt', 'basic_regress_cnt','basic_regress_both','sctransform','sctransform_regress','scran'")
+}
---
+++ seurat_processing.R
@@ -0,0 +1,197 @@
+## Seurat V3 analysis
+## initialize object
+
+library(Seurat)
+library(dplyr)
+
+
+### load in functions
+source("/wynton/group/reiter/lauren/seurat_scripts/seurat_functions.R")
+source("/wynton/group/reiter/lauren/seurat_scripts/seurat_analyses.R")
+
+args=commandArgs(trailingOnly = T)
+
+basename=args[1]
+version=args[2]
+parameters_file = args[3]
+do_marks = args[4]
+
+setwd(basename)
+
+### load in parameter arguments
+parameters = read.csv(parameters_file,sep=",",header=T)
+parameter_checks(parameters)
+
+horm = parameters$human_mouse
+MT_filter = parameters$Mt_filter
+nCount_high = parameters$nCount_high
+nCount_low = parameters$nCount_low
+nFeature_high = parameters$nFeature_high
+nFeature_low = parameters$nFeature_low
+num_pcs = parameters$PCs
+res=parameters$resolution
+doublets = parameters$path_to_doublet_csv 
+analyses=parameters$analyses
+
+## read in data
+data_dir ="filtered_feature_bc_matrix/"
+data = Read10X(data.dir=data_dir)
+log_file = paste(basename,version,analyses,"log.txt",sep="_")
+
+## create version folder and set directory
+if(!dir.exists(version)){
+        dir.create(version)
+        setwd(version)
+        sink(file=log_file)
+        print("Created new directory")
+        
+}else{
+        setwd(version)
+        sink(file=log_file)
+        print("Version directory already exists")
+        
+        
+}
+
+if(!dir.exists("QCplots")){
+        dir.create("QCplots")
+        print("Created new QCplots directory")
+        
+}
+
+print("initializing Seurat object")
+## initialize seurat object
+seur <- CreateSeuratObject(counts = data, project = basename, min.cells = 3, min.features = 200)
+
+if(horm == "mouse"){
+        seur[["percent.mt"]] <- PercentageFeatureSet(object = seur, pattern = "^mt-")
+        print("Using pattern '^mt-' for mouse")
+} else{
+        seur[["percent.mt"]] <- PercentageFeatureSet(object = seur, pattern = "^MT-")
+        print("Using pattern '^MT-' for human")
+}
+
+########################################################################
+## to subset or not to subset?
+print("Subsetting based on filters")
+if(sum(!is.na(c(MT_filter, nCount_high,nFeature_high))) == 0 & sum(nCount_low,nFeature_low)==0){
+        print("No Subsetting")
+}else if(sum(!is.na(c(MT_filter, nCount_high,nFeature_high))) == 0){
+        seur=subset(seur, subset = nCount_RNA > nCount_low & nFeature_RNA > nFeature_low)
+        print("Subsetted only on nCount_low and nFeature_low")
+}else if(!is.na(MT_filter)){
+        seur = subset(seur, subset = percent.mt <MT_filter & nCount_RNA > nCount_low & nFeature_RNA > nFeature_low)
+        if(!is.na(nCount_high) & !is.na(nFeature_high)){
+                seur=subset(seur, subset = nCount_RNA < nCount_high)
+                seur=subset(seur, subset=nFeature_RNA < nFeature_high)
+                print("Subsetted on percent.MT, nCount, and nFeature")
+        }else if(!is.na(nCount_high)){
+                seur=subset(seur, subset = nCount_RNA < nCount_high)
+                print("Subsetted on percent.MT, nCount, nFeature_low")
+        }else if(!is.na(nFeature_high)){
+                seur=subset(seur, subset=nFeature_RNA < nFeature_high)
+                print("Subsetted on percent.MT, nFeature, and nCount_low")
+        }else{
+                print("Subsetted on percent.MT, nCount_low, and nFeature_low")
+        }
+}else if(!is.na(nCount_high) & !is.na(nFeature_high)){
+        seur=subset(seur, subset = nCount_RNA < nCount_high & nCount_RNA > nCount_low)
+        seur=subset(seur, subset=nFeature_RNA < nFeature_high & nFeature_RNA > nFeature_low)
+        print("Subsetted on nCount and nFeature")
+}else if(!is.na(nCount_high)){
+        seur=subset(seur, subset = nCount_RNA < nCount_high & nCount_RNA > nCount_low & nFeature_RNA > nFeature_low)
+        print("Subsetted on nCount nad nFeature_low")
+}else{
+        seur=subset(seur, subset=nFeature_RNA < nFeature_high & nFeature_RNA > nFeature_low & nCount_RNA > nCount_low)
+        print("Subsetted on nFeature and nCount_low")
+}
+
+sink()
+
+setwd("QCplots/")
+qc_plots_stats(seur, basename, version)
+setwd("../")
+
+#######################################################################
+## taking care of doublets (everyday!)
+
+if(is.na(doublets)){
+        sink(file=log_file,append=T)
+        print("Not removing any doublets")
+        sink()
+}else{## need to find example file and fill this out
+}
+
+
+#######################################################################
+print("starting normalization")
+seur <- NormalizeData(seur, normalization.method = "LogNormalize", scale.factor = 10000)
+seur <- FindVariableFeatures(seur, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(seur), 10)
+plot1_vg <- VariableFeaturePlot(seur)
+plot2_vg <- LabelPoints(plot = plot1_vg, points = top10, repel = TRUE)
+
+png(paste0("QCplots/",basename,"_",version,"_variablegenes.png"),height = 800,width=1100)
+CombinePlots(plots = list(plot1_vg, plot2_vg))
+dev.off()
+
+######################################################################
+## Seurat analyses
+
+sink(log_file,append=T)
+print(paste0("Analyses to perform: ", analyses))
+sink()
+
+if("all" %in% analyses){
+        sink(file=log_file,append=T,split=T)
+        print("Running all analyses")
+        basic_noregress(seur,basename,version,res,num_pcs,do_marks)
+        print("Finished 'basic'")
+        basic_regress_pcmt(seur,basename,version,res,num_pcs,do_marks)
+        print("Finished 'basic_regress_pcmt'")
+        basic_regress_both(seur,basename,version,res,num_pcs,do_marks)
+        print("Finished 'basic_regress_both'")
+        basic_regress_count(seur,basename,version,res,num_pcs,do_marks)
+        print("Finished 'basic_regress_cnt'")
+        sctrans(data,basename,version,res,num_pcs,do_marks)
+        print("Finished 'sctransform'")
+        sctrans_regress(data,basename,version,res,num_pcs,do_marks)
+        print("Finished 'sctransform_regress'")
+        scran_analysis(seur,basename,version,res,num_pcs,do_marks)
+        print("Finished 'scran'")
+        sink()
+}else for(analysis in list_of_analyses(analyses)){
+        sink(file=log_file,append=T,split=T)
+        if(analysis == "basic"){
+                print("Starting 'Basic' analysis")
+                basic_noregress(seur,basename,version,res,num_pcs,do_marks)
+                print("Finished 'Basic' analysis")
+        }else if(analysis =="basic_regress_pcmt"){
+                print("Starting 'Basic_regress_pcmt' analysis")
+                basic_regress_pcmt(seur,basename,version,res,num_pcs,do_marks)
+                print("Finished 'Basic_regress_pcmt' analysis")
+        }else if(analysis == "basic_regress_cnt"){
+                print("Starting 'Basic_regress_cnt' analysis")
+                basic_regress_count(seur,basename,version,res,num_pcs,do_marks)
+                print("Finished 'Basic_regress_cnt' analysis")
+        }else if(analysis == "basic_regress_both"){
+                print("Starting 'Basic_regress_both' analysis")
+                basic_regress_both(seur,basename,version,res,num_pcs,do_marks)
+                print("Finished'Basic_regress_both' analysis")
+        }else if(analysis == "sctransform"){
+                print("Starting 'sctransform' analysis")
+                sctrans(data,basename,version,res,num_pcs,do_marks)
+                print("Finished 'sctransform' analysis")
+        }else if(analysis == "sctransform_regress"){
+                print("Starting 'sctransform_regress' analysis")
+                sctrans_regress(data,basename,version,res,num_pcs,do_marks)
+                print("Finished 'sctransform_regress' analysis")
+        }else if(analysis == "scran"){
+                print("Starting 'scran' analysis")
+                scran_analysis(seur,basename,version,res,num_pcs,do_marks)
+                print("Finished 'scran' analysis")
+        }
+        sink()
+}
+
+
