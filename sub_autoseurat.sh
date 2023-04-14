902f3e9934c19fc549d92595296b7571d2c65b5e
lb15 <40410297+lb15@users.noreply.github.com>
2023-04-14 15:14:00 -0700

    update submission script

--- sub_autoseurat.sh
+++ sub_autoseurat.sh
@@ -2,7 +2,7 @@
 #                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                     #-- the shell for the job
 #$ -o /wynton/group/reiter/lauren/log_autoS                        #-- output directory (fill in)
-#$ -e /wynton/group/reiter/lauren/log_autoS                         #-- error directory (fill in)
+#$ -e /wynton/group/reiter/lauren/log_autoS                        #-- error directory (fill in)
 #$ -cwd                            #-- tell the job that it should start in your working directory
 #$ -r y                            #-- tell the system that if a job crashes, it should be restarted
 #$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
@@ -10,10 +10,9 @@
 #$ -l scratch=50G
 #$ -l h_rt=72:00:00
 #$ -m ea                           #--email when done
-#$ -M Lauren.Byrnes@ucsf.edu        #--email
 
 module load CBI
-module load r/4.0.3
+module load r/4.0.5
 
 if [[ -z "$TMPDIR" ]]; then
   if [[ -d /scratch ]]; then TMPDIR=/scratch/$USER; else TMPDIR=/tmp/$USER; fi
@@ -28,15 +27,18 @@ basename=$1
 version=$2
 para="$1"_"$2"_parameters.csv
 do_marks=$3
+dir=$4
 script=/wynton/group/reiter/lauren/autoSeurat/seurat_processing.R
+
+if [[ $dir == "" ]];then dir=/wynton/group/reiter/lauren; fi
 
 mkdir $basename
 
-cp -r /wynton/group/reiter/lauren/$basename/outs/filtered_feature_bc_matrix $basename/
-cp /wynton/group/reiter/lauren/$basename/$para $basename/
+cp -r $dir/$basename/outs/filtered_feature_bc_matrix $basename/
+cp $dir/$basename/$para $basename/
 
 Rscript $script $basename $version $para $do_marks
 
-rsync -azvrP --update $basename/$version /wynton/group/reiter/lauren/$basename/testseurat
+rsync -azvrP --update $basename/$version $dir/$basename/
 
 [[ -n "$JOB_ID" ]] && qstat -j "$JOB_ID"
